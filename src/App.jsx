da88fe7ab8d0116089c61b6ad3a2394a013b67e9
vedcer20 <vedant.gupta.cer20@iitbhu.ac.in>
2025-07-19 12:16:11 +0530

    use images

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,13 @@ import Services from "./components/Services";
 import Features from "./components/Features";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import Toggle from "./components/Toggle";
 import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
-  const [theme, setTheme] = useState("dark");
-
-  useEffect(() => {
-    document.documentElement.setAttribute("data-theme", theme);
-  }, [theme]);
-
   return (
     <div>
       <Navbar />
-      <Toggle theme={theme} setTheme={setTheme} />
       <Hero />
       <About />
       <Services />
